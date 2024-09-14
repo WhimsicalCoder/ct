3f237683e5ca62f2eddf108eba6327f05bfa0668
WhimsicalCoder <91513087+WhimsicalCoder@users.noreply.github.com>
2024-09-14 14:41:39 -0400

    Add files via upload

---
+++ middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "d4bac144-4021-4293-8559-dba70d8bb8e6");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
